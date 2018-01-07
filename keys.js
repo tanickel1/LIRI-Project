7d297663250ccdfa936a485a703b64562543df74
tanickel1 <tanickel1@gmail.com>
2018-01-07 14:56:07 -0500

    Added steps up to the point of creating liri app

---
+++ keys.js
@@ -0,0 +1,16 @@
+console.log('this is loaded');
+
+exports.twitter = {
+  consumer_key: process.env.N038Mu8IDKVywQS0NDEwey2TN,
+  consumer_secret: process.env.9J0KbjS3p1zzuieVychGmFQyGeRgahMAfrtQ9bJhyH5zGpLilM,
+  access_token_key: process.env.950072176434188290-szkPTaNtu2iun61yiGjTrcpL2OW7NjU,
+  access_token_secret: process.env.Wf5Y8Zcm5ucvYys3xFoJ3EC0rGNsQxw7MkmQH6FWaRTyY
+};
+
+exports.spotify = {
+  id: process.env.4ad0cacb85034c40846dd12a4eb8f6cd,
+  secret: process.env.4d7bc550f3a14953948ad462d8221f4f
+};
+
+
+
